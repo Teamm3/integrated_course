2eae858fd5c3acd88f9409d7d3efdf880190e2ec
dzqann <dzq_college@163.com>
2019-12-04 15:15:32 +0800

    封了一下对灯的控制

--- main.c
+++ main.c
@@ -31,6 +31,13 @@
 #include <stdlib.h>
 /* USER CODE END Includes */
 
+#define RED_LIGHT 1
+#define BLUE_LIGHT 2
+#define GREEN_LIGHT 3
+
+#define LIGHT_ON 1
+#define LIGHT_OFF 0
+
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 
@@ -66,24 +73,32 @@ int KE1_Parse_NNMI(char *pcNNMI, char *pcOut)
   char acLen[5] = {0};
   int i = 0, pos = 0, dLen = 0;
   //+NNMI:6,010009020000
-  for(i=0; i<strlen(pcNNMI); i++){
+  for (i = 0; i < strlen(pcNNMI); i++)
+  {
     ch = pcNNMI[i];
-    if(0x0D == ch && 1 == dataFlag) break;
-    
-    if(':' == ch){
-      lenFlag = 1; pos = 0;
+    if (0x0D == ch && 1 == dataFlag)
+      break;
+
+    if (':' == ch)
+    {
+      lenFlag = 1;
+      pos = 0;
       continue;
     }
-    if(',' == ch){
+    if (',' == ch)
+    {
       lenFlag = 0;
-      dataFlag = 1; pos = 0;
+      dataFlag = 1;
+      pos = 0;
       dLen = atoi(acLen);
       continue;
     }
-    if(1 == lenFlag){
+    if (1 == lenFlag)
+    {
       acLen[pos++] = ch;
     }
-    if(1 == dataFlag){
+    if (1 == dataFlag)
+    {
       pcOut[pos++] = ch;
     }
   }
@@ -94,10 +109,49 @@ void ascii2hex(char *pcAscii, char *pcHex)
 {
   int iDlen = 0, i = 0, pos = 0;
   iDlen = strlen(pcAscii);
-  if(128 < iDlen) return;
-  for(i=0; i<iDlen; i++){
+  if (128 < iDlen)
+    return;
+  for (i = 0; i < iDlen; i++)
+  {
     sprintf(&pcHex[pos], "%02X", pcAscii[i]);
     pos += 2;
+  }
+}
+
+int hex2dec(char tens, char unit)
+{
+  int result = 0;
+  if (tens >= 'A' && tens <= 'Z')
+    result = tens - 'A' + 10;
+  else
+    result = tens - '0';
+  result = result * 16;
+  if (unit >= 'A' && unit <= 'Z')
+    result = result + unit - 'A';
+  else
+    result = result + unit - '0';
+  return result;
+}
+
+void ctrl_light(int light_number, int ctrl_number)
+{
+  switch (light_number)
+  {
+  case BLUE_LIGHT:
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 0);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 1);
+    break;
+  case RED_LIGHT:
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 0);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 1);
+    break;
+  case GREEN_LIGHT:
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 0);
+    break;
   }
 }
 
@@ -111,12 +165,11 @@ char acDevInfo[128] = {0}, acHexBuf[256] = {0}, acAtBuf[512] = {0}, acUserCmd[64
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-  float fTemp = 34.2, fHumi = 0.0;  //温度和湿度
+  float fTemp = 34.2, fHumi = 0.0; //温度和湿度
   int iUserCase = 0, iRet = -1;
   char netFlag = 0, cmdLen = 0;
   uint32_t atLen = 0, dLen = 0, timeout = 1000, upFreq = 0;
   /* USER CODE END 1 */
-  
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -141,13 +194,13 @@ int main(void)
   MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
   UART_Enable_Receive_IT();
-  
+
   printf("Hello! i am KE1 south demo\r\n");
-  
+
   KE1_I2C_SHT31(&fTemp, &fHumi);
-  
+
   printf("T:%0.2f, H:%0.2f%%\r\n", fTemp, fHumi);
-  
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -158,148 +211,140 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
     printf("iUserCase %d\r\n", iUserCase);
-    switch(iUserCase){
-      case 0:
-        KE1_Clear_AT_Buf();
-        iUserCase = 1;
-        break;
-      case 1:
-        KE1_Send_AT("AT\r\n"); // AT测试模组
-        break;
-      case 2:
-        KE1_Send_AT("AT+CFUN=0\r\n"); // 关闭模组
-        timeout = 10000;
-        break;
-      case 3:
-        KE1_Send_AT("AT+NCDP=180.101.147.115,5683\r\n"); // 设置电信物联网南向接口地址
-        break;
-      case 4:
-        KE1_Send_AT("AT+CFUN=1\r\n"); // 启动模块
-        timeout = 10000;
-        break;
-      case 5:
-        KE1_Send_AT("AT+CGDCONT=1,\"IP\",\"CTNB\"\r\n"); // 设置PDP
-        timeout = 1000;
-        break;
-      case 6:
-        KE1_Send_AT("AT+CGATT=1\r\n"); // 模块弿始联罿
-        break;
-      #if 1
-      case 7:
-        if(0 == upFreq && 1 == netFlag){
-          KE1_I2C_SHT31(&fTemp, &fHumi); // 采集温湿度或者其他信息上传到电信物联网平台
-        
-          memset(acDevInfo, 0, sizeof(acDevInfo));
-          memset(acAtBuf, 0, sizeof(acAtBuf));
-        
-          dLen = snprintf(acDevInfo, sizeof(acDevInfo), "{\"T\":\"%0.2f\",\"H\":\"%0.2f\"}", fTemp, fHumi);// 打包用户数据
-          printf("%s\r\n", acDevInfo);
-          ascii2hex(acDevInfo, acHexBuf);
-          snprintf(acAtBuf, sizeof(acAtBuf), "AT+NMGS=%d,00%04X%s\r\n", (dLen+3), dLen, acHexBuf);// 打包COAP数据包AT命令
-          printf("%s\r\n", acAtBuf);
-          KE1_Send_AT(acAtBuf);
-          upFreq = (60*60);
-        }
-        break;
-        #endif
-      
+    switch (iUserCase)
+    {
+    case 0:
+      KE1_Clear_AT_Buf();
+      iUserCase = 1;
+      break;
+    case 1:
+      KE1_Send_AT("AT\r\n"); // AT测试模组
+      break;
+    case 2:
+      KE1_Send_AT("AT+CFUN=0\r\n"); // 关闭模组
+      timeout = 10000;
+      break;
+    case 3:
+      KE1_Send_AT("AT+NCDP=180.101.147.115,5683\r\n"); // 设置电信物联网南向接口地址
+      break;
+    case 4:
+      KE1_Send_AT("AT+CFUN=1\r\n"); // 启动模块
+      timeout = 10000;
+      break;
+    case 5:
+      KE1_Send_AT("AT+CGDCONT=1,\"IP\",\"CTNB\"\r\n"); // 设置PDP
+      timeout = 1000;
+      break;
+    case 6:
+      KE1_Send_AT("AT+CGATT=1\r\n"); // 模块弿始联罿
+      break;
+#if 1
+    case 7:
+      if (0 == upFreq && 1 == netFlag)
+      {
+        KE1_I2C_SHT31(&fTemp, &fHumi); // 采集温湿度或者其他信息上传到电信物联网平台
+
+        memset(acDevInfo, 0, sizeof(acDevInfo));
+        memset(acAtBuf, 0, sizeof(acAtBuf));
+
+        dLen = snprintf(acDevInfo, sizeof(acDevInfo), "{\"T\":\"%0.2f\",\"H\":\"%0.2f\"}", fTemp, fHumi); // 打包用户数据
+        printf("%s\r\n", acDevInfo);
+        ascii2hex(acDevInfo, acHexBuf);
+        snprintf(acAtBuf, sizeof(acAtBuf), "AT+NMGS=%d,00%04X%s\r\n", (dLen + 3), dLen, acHexBuf); // 打包COAP数据包AT命令
+        printf("%s\r\n", acAtBuf);
+        KE1_Send_AT(acAtBuf);
+        upFreq = (60 * 60);
+      }
+      break;
+#endif
     }
-    
+
     atLen = sizeof(acAtBuf);
     iRet = KE1_Recv_AT(acAtBuf, &atLen, timeout);
     //printf("\r\nRAT:%d\r\n",iRet);
-    if(0 == iRet){//AT命令响应超时
-    
-    }else if(1 == iRet){//AT命令接收到OK响应
-      if(7 == iUserCase){
-      
-      }else{
+    if (0 == iRet)
+    { //AT命令响应超时
+    }
+    else if (1 == iRet)
+    { //AT命令接收到OK响应
+      if (7 == iUserCase)
+      {
+      }
+      else
+      {
         iUserCase++;
       }
-      
+
       HAL_Delay(1000);
-    }else if(2 == iRet){//AT命令接收到ERROR响应
+    }
+    else if (2 == iRet)
+    { //AT命令接收到ERROR响应
       printf("AT error !\r\n");
-      if(7 == iUserCase){
+      if (7 == iUserCase)
+      {
         iUserCase = 0; // 重新初始化模组
       }
       HAL_Delay(1000);
-    }else if(3 == iRet){//接收到网络已注册提示
+    }
+    else if (3 == iRet)
+    { //接收到网络已注册提示
       printf("Net ready !\r\n");
       netFlag = 1;
       HAL_Delay(5000);
-    }else if(4 == iRet){//AT命令接收到电信物联网平台下发数据
+    }
+    else if (4 == iRet)
+    { //AT命令接收到电信物联网平台下发数据
       printf("%s", acAtBuf);
       memset(acUserCmd, 0, sizeof(acUserCmd));
       cmdLen = KE1_Parse_NNMI(acAtBuf, acUserCmd);
       printf("user data[%d]:%s\r\n", cmdLen, acUserCmd);
-      if(strstr(acUserCmd, "AAAA0000")){
+      if (strstr(acUserCmd, "AAAA0000"))
+      {
         printf("device info upload successfully\r\n");
-      }else{
+      }
+      else
+      {
         /* 
         * 解析用户命令执行对应操作 
         * TO-DO
         */
-       char tens = acUserCmd[6];
-       char unit = acUserCmd[7];
-       int cmd_num = (tens - '0') * 16 + (unit - '0');
-       int flag = 0;
-       switch (cmd_num)
-       {
-       case 0: 
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 0);
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 1);
-            break;
+        int cmd_num = hex2dec(acUserCmd[6], acUserCmd[7]);
+        switch (cmd_num)
+        {
+        case 0:
+          ctrl_light(RED_LIGHT, LIGHT_ON);
+          break;
         case 1:
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 0);
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 1);
-            break;
+          ctrl_light(RED_LIGHT, LIGHT_OFF);
+          break;
         case 2:
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 0);
-            break;
+          ctrl_light(GREEN_LIGHT, LIGHT_ON);
+          break;
         case 3:
-            flag = 1;
-            while(flag == 1){
-                HAL_Delay(1000);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 0);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 1);
-                HAL_Delay(1000);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 0);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 1);
-                HAL_Delay(1000);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
-                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 0);
-            }
+          ctrl_light(GREEN_LIGHT, LIGHT_OFF);
+          break;
         case 4:
-            flag = 0;
-        default:
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 0);
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 0);
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 0);
-            break;
+          ctrl_light(BLUE_LIGHT, LIGHT_ON);
+        case 5:
+          ctrl_light(BLUE_LIGHT, LIGHT_OFF);
+        }
 
-
-       }
-        
         /* 向平台发送命令响应
         * AT+NMGS=5,02000A000A
         */
         acUserCmd[1] = '2';
-        acUserCmd[6] = '0';acUserCmd[7] = '0';acUserCmd[8] = '0';acUserCmd[9] = '0';acUserCmd[10] = 0;
-        snprintf(acAtBuf, sizeof(acAtBuf), "AT+NMGS=%d,%s\r\n", 5, acUserCmd);// 打包COAP数据包AT命令
+        acUserCmd[6] = '0';
+        acUserCmd[7] = '0';
+        acUserCmd[8] = '0';
+        acUserCmd[9] = '0';
+        acUserCmd[10] = 0;
+        snprintf(acAtBuf, sizeof(acAtBuf), "AT+NMGS=%d,%s\r\n", 5, acUserCmd); // 打包COAP数据包AT命令
         KE1_Send_AT(acAtBuf);
       }
-      
     }
-    
-    if(0 != upFreq) upFreq--;
+
+    if (0 != upFreq)
+      upFreq--;
   }
   /* USER CODE END 3 */
 }
@@ -331,8 +376,7 @@ void SystemClock_Config(void)
   }
   /** Initializes the CPU, AHB and APB busses clocks 
   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -342,8 +386,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_USART3
-                              |RCC_PERIPHCLK_I2C2;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_USART3 | RCC_PERIPHCLK_I2C2;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
   PeriphClkInit.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
   PeriphClkInit.I2c2ClockSelection = RCC_I2C2CLKSOURCE_PCLK1;
@@ -375,7 +418,7 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
@@ -384,7 +427,7 @@ void Error_Handler(void)
   * @retval None
   */
 void assert_failed(char *file, uint32_t line)
-{ 
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
