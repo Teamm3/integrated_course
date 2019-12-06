9727ce61242f80a3e20de101ad1cfbfc62786ae9
dzqann <dzq_college@163.com>
2019-12-06 09:55:08 +0800

    light sound

--- main.c
+++ main.c
@@ -26,20 +26,18 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-/* USER CODE END Includes */
-
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 #define RED_LIGHT 1
 #define BLUE_LIGHT 2
 #define GREEN_LIGHT 3
 
-#define LIGHT_ON 1
-#define LIGHT_OFF 0
+#define LIGHT_ON 0
+#define LIGHT_OFF 1
+/* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
@@ -68,8 +66,6 @@ void SystemClock_Config(void);
 
 /* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
 int KE1_Parse_NNMI(char *pcNNMI, char *pcOut)
 {
   char ch = 0, lenFlag = 0, dataFlag = 0;
@@ -163,12 +159,13 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
   static uint16_t ausAdcDataBuf[2] = {0};
+  uint16_t val = 0;
+  float fVolts = 0.0, fAmps = 0.0, fMicroamps = 0.0, fLux = 0.0;
+
   float fTemp = 34.2, fHumi = 0.0;
   int iUserCase = 0, iRet = -1;
   char netFlag = 0, cmdLen = 0;
-  uint16_t val = 0;
   uint32_t atLen = 0, dLen = 0, timeout = 1000, upFreq = 0;
-  float fVolts = 0.0, fAmps = 0.0, fMicroamps = 0.0, fLux = 0.0;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -189,13 +186,16 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USART1_UART_Init();
   MX_USART3_UART_Init();
   MX_I2C2_Init();
   MX_TIM2_Init();
   MX_DMA_Init();
   MX_ADC1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
+  printf("Hello ADC\r\n");
+
+  /* USER CODE END 2 */
   UART_Enable_Receive_IT();
 
   printf("Hello! i am KE1 south demo\r\n");
@@ -203,9 +203,6 @@ int main(void)
   KE1_I2C_SHT31(&fTemp, &fHumi);
 
   printf("T:%0.2f, H:%0.2f%%\r\n", fTemp, fHumi);
-
-  /* USER CODE END 2 */
-
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
@@ -221,25 +218,25 @@ int main(void)
       iUserCase = 1;
       break;
     case 1:
-      KE1_Send_AT("AT\r\n"); // AT测试模组
+      KE1_Send_AT("AT\r\n"); // AT????
       break;
     case 2:
-      KE1_Send_AT("AT+CFUN=0\r\n"); // 关闭模组
+      KE1_Send_AT("AT+CFUN=0\r\n"); // ????
       timeout = 10000;
       break;
     case 3:
-      KE1_Send_AT("AT+NCDP=180.101.147.115,5683\r\n"); // 设置电信物联网南向接口地址
+      KE1_Send_AT("AT+NCDP=180.101.147.115,5683\r\n"); // ?????????????
       break;
     case 4:
-      KE1_Send_AT("AT+CFUN=1\r\n"); // 启动模块
+      KE1_Send_AT("AT+CFUN=1\r\n"); // ????
       timeout = 10000;
       break;
     case 5:
-      KE1_Send_AT("AT+CGDCONT=1,\"IP\",\"CTNB\"\r\n"); // 设置PDP
+      KE1_Send_AT("AT+CGDCONT=1,\"IP\",\"CTNB\"\r\n"); // ??PDP
       timeout = 1000;
       break;
     case 6:
-      KE1_Send_AT("AT+CGATT=1\r\n"); // 模块弿始联罿
+      KE1_Send_AT("AT+CGATT=1\r\n"); // ??????
       break;
 #if 1
     case 7:
@@ -254,12 +251,12 @@ int main(void)
         fAmps = fVolts / 10000.0f;
         fMicroamps = fAmps * 1000000;
         fLux = fMicroamps * 2.0f;
-        // printf("Light:PA4_ADC:%d - %.2fLux\n", ausAdcDataBuf[0], fLux);
+        printf("Light:PA4_ADC:%d - %.2fLux\n", ausAdcDataBuf[0], fLux);
 
         //
         val = ausAdcDataBuf[1];
         val = (((3.3f * val) / 4096.0f) * 2 * 1.5f) * 1000;
-        // printf("Sound:PA6_ADC:%d - %d\n", ausAdcDataBuf[1], val);
+        printf("Sound:PA6_ADC:%d - %d\n", ausAdcDataBuf[1], val);
 
         HAL_ADC_Stop(&hadc1);
 
@@ -268,7 +265,7 @@ int main(void)
         memset(acDevInfo, 0, sizeof(acDevInfo));
         memset(acAtBuf, 0, sizeof(acAtBuf));
 
-        dLen = snprintf(acDevInfo, sizeof(acDevInfo), "{\"T\":\"%0.2f\",\"H\":\"%0.2f\",\"L\":\"%0.2f\",\"S\":\"%0.2f\"}", fTemp, fHumi, fLux, val); // 打包用户数据
+        dLen = snprintf(acDevInfo, sizeof(acDevInfo), "{\"T\":\"%0.2f\",\"H\":\"%0.2f\",\"L\":\"%0.2f\",\"S\":\"%d\"}", fTemp, fHumi, fLux, val); // ??????
         printf("%s\r\n", acDevInfo);
         ascii2hex(acDevInfo, acHexBuf);
         snprintf(acAtBuf, sizeof(acAtBuf), "AT+NMGS=%d,00%04X%s\r\n", (dLen + 3), dLen, acHexBuf); // æ‰“åŒ…COAPæ•°æ®åŒ…ATå‘½ä»¤
@@ -285,10 +282,10 @@ int main(void)
     iRet = KE1_Recv_AT(acAtBuf, &atLen, timeout);
     //printf("\r\nRAT:%d\r\n",iRet);
     if (0 == iRet)
-    { //AT命令响应超时
+    { //AT??????
     }
     else if (1 == iRet)
-    { //AT命令接收到OK响应
+    { //AT?????OK??
       if (7 == iUserCase)
       {
       }
@@ -300,22 +297,22 @@ int main(void)
       HAL_Delay(1000);
     }
     else if (2 == iRet)
-    { //AT命令接收到ERROR响应
+    { //AT?????ERROR??
       printf("AT error !\r\n");
       if (7 == iUserCase)
       {
-        iUserCase = 0; // 重新初始化模组
+        iUserCase = 0; // ???????
       }
       HAL_Delay(1000);
     }
     else if (3 == iRet)
-    { //接收到网络已注册提示
+    { //??????????
       printf("Net ready !\r\n");
       netFlag = 1;
       HAL_Delay(5000);
     }
     else if (4 == iRet)
-    { //AT命令接收到电信物联网平台下发数据
+    { //AT????????????????
       printf("%s", acAtBuf);
       memset(acUserCmd, 0, sizeof(acUserCmd));
       cmdLen = KE1_Parse_NNMI(acAtBuf, acUserCmd);
@@ -327,7 +324,7 @@ int main(void)
       else
       {
         /* 
-        * 解析用户命令执行对应操作 
+        * ???????????? 
         * TO-DO
         */
         int cmd_num = hex2dec(acUserCmd[6], acUserCmd[7]);
@@ -362,7 +359,7 @@ int main(void)
           ctrl_light(GREEN_LIGHT, LIGHT_ON);
         }
 
-        /* 向平台发送命令响应
+        /* ?????????
         * AT+NMGS=5,02000A000A
         */
         acUserCmd[1] = '2';
@@ -371,7 +368,7 @@ int main(void)
         acUserCmd[8] = '0';
         acUserCmd[9] = '0';
         acUserCmd[10] = 0;
-        snprintf(acAtBuf, sizeof(acAtBuf), "AT+NMGS=%d,%s\r\n", 5, acUserCmd); // 打包COAP数据包AT命令
+        snprintf(acAtBuf, sizeof(acAtBuf), "AT+NMGS=%d,%s\r\n", 5, acUserCmd); // ??COAP???AT??
         KE1_Send_AT(acAtBuf);
       }
     }
@@ -419,10 +416,8 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_USART3 | RCC_PERIPHCLK_I2C2;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_ADC;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
-  PeriphClkInit.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
-  PeriphClkInit.I2c2ClockSelection = RCC_I2C2CLKSOURCE_PCLK1;
   PeriphClkInit.AdcClockSelection = RCC_ADCCLKSOURCE_PLLSAI1;
   PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSE;
   PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
